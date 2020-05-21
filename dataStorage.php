8aaed83a6c1f59bcd8aed2f2eb4fb3020366d3b9
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2020-05-21 06:48:29 -0700

    Create dataStorage.php

---
+++ dataStorage.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["Fname"]; ?><br>
+Total Left Wheel Count: <?php echo $_POST["Lname"]; ?>
+Total Right Wheel Count: <?php echo $_POST["Rname"]; ?>
+Total Duration: <?php echo $_POST["Tname"]; ?>
+
+</body>
+</html>
